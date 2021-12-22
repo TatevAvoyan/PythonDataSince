69389e8f73242cc82719acee354e7dcd49119b01
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-23 01:23:04 +0400

    Infection Cases

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import numpy as np
 
-data = np.arange(1, 100)
+data = np.array([120, 98, 150, 65, 42, 100, 190, 220, 140, 110, 88, 89, 100, 120, 50, 180, 155, 42, 89, 77, 200, 190, 125, 98, 77, 40, 39, 59, 30, 67])
 
-print(data[(data % 3 == 0) & (data % 5 == 0)])
+mean = np.mean(data)
+new = np.array([])
+
+for i in data:
+    if mean < i:
+        new = np.append(new, i)
+
+print(new.size)
