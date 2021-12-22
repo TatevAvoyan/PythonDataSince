2ab8ae202c2f4ef9540423153ecaea1e1b7d1cbf
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-23 01:54:17 +0400

    House price (std, mean, size)

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+# You are given an array that represents house prices.
+# Calculate and output the percentage of houses that are within one standard deviation from the mean.
+
+
 import numpy as np
 
-data = np.array([120, 98, 150, 65, 42, 100, 190, 220, 140, 110, 88, 89, 100, 120, 50, 180, 155, 42, 89, 77, 200, 190, 125, 98, 77, 40, 39, 59, 30, 67])
-
-mean = np.mean(data)
-new = np.array([])
+data = np.array([150000, 125000, 320000, 540000, 200000, 120000, 160000, 230000, 280000, 290000, 300000, 500000, 420000, 100000, 150000, 280000])
+new = []
+std = int(np.std(data))
+mean = int(np.mean(data))
+low = mean - std
+high = mean + std
 
 for i in data:
-    if mean < i:
-        new = np.append(new, i)
 
-print(new.size)
+    if i > low and i < high:
+        new.append(i)
+
+print((len(new) / data.size) * 100)
+
