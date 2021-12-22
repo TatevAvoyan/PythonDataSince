d3865832a15ca5073fd126f35a5e278ecc4c9231
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-23 00:55:02 +0400

    test

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-# The given code includes a list of heights for various basketball players.
-# You need to calculate and output how many players are in the range of one standard deviation from the mean.
 import numpy as np
 
+data = np.arange(1, 100)
 
-def players_count():
-    players = [180, 172, 178, 185, 190, 195, 192, 200, 210, 190]
-
-    a = np.array(players)
-    mean = np.sum(a)/a.size  # 189.2
-    variance = np.sum((a - mean)**2) / a.size  # 111.56
-    std = variance ** (1/2)  # 10.56
-    b = int(mean - std)
-    c = int(mean + std)
-    count = 0
-    for i in range(b, c):
-        if i in players:
-            count += 1
-    return count
-
-
-print(players_count())
+print(data[(data % 3 == 0) & (data % 5 == 0)])
