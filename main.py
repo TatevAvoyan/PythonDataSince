1ad2ef676ad051525888bce52fdd9a38850b9808
Tatev Avoyan <tatev.avoyan@yahoo.com>
2021-12-23 01:01:59 +0400

    Basketball Players, (mean, variance, std, count, version 2 NumPy)

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ def players_count():
     players = [180, 172, 178, 185, 190, 195, 192, 200, 210, 190]
 
     a = np.array(players)
-    mean = np.sum(a)/a.size  # 189.2
-    variance = np.sum((a - mean)**2) / a.size  # 111.56
-    std = variance ** (1/2)  # 10.56
+    mean = np.sum(a)/a.size  # 189.2 np.mean(players)
+    variance = np.sum((a - mean)**2) / a.size  # 111.56 np.var(players)
+    std = variance ** (1/2)  # 10.56 np.std(players)
     b = int(mean - std)
     c = int(mean + std)
     count = 0
